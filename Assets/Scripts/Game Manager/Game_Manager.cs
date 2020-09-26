994108332b853a6b5c1f97bfde0064111e90fc76
Kaeden <kaeden@bcsengineering.com>
2020-09-26 01:32:10 -0400

    Dog actually exists
    Game Manager code fixed

---
+++ Assets/Scripts/Game Manager/Game_Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{
+    public bool dog_active;
+    public Movement_FPS dog_controller;
+    public Movement_FPS player_controller;
+
+    void Update()
+    {
+        switch (dog_active)
+        {
+            case true:
+                dog_controller.enabled = true;
+                player_controller.enabled = false;
+                break;
+            case false:
+                dog_controller.enabled = false;
+                player_controller.enabled = true;
+                break;
+        }
+    }
+}
