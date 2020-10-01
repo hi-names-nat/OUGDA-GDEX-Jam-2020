acf756801f73b547dcd4da68f996f47a88dc3508
Kaeden <kaeden@bcsengineering.com>
2020-10-01 14:38:09 -0400

    Switching works
    Nat im sorry but i cannot use the new input manager it doesnt make any sense and i cant get it to work, i made everything happen with the old system though, i just need to add layer masks to the dog and jumping to the player

---
+++ Assets/Scripts/Game Manager/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject playerController;
+    public GameObject dogController;
+    public GameObject playerCamera;
+    public GameObject dogCamera;
+
+    public bool dogActive = false;
+
+    private void Awake()
+    {
+        DogSwitch();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("DogSwitch"))
+        {
+            dogActive = !dogActive;
+            DogSwitch();
+        }
+    }
+
+    void DogSwitch()
+    {
+        switch (dogActive)
+        {
+            case true:
+                // Disable player components
+                playerController.GetComponent<Movement>().enabled = false;
+                playerCamera.SetActive(false);
+                
+                // Enable dog components
+                dogController.GetComponent<Movement>().enabled = true;
+                dogCamera.SetActive(true);
+                break;
+            case false:
+                // Enable player components
+                playerController.GetComponent<Movement>().enabled = true;
+                playerCamera.SetActive(true);
+
+                // Disable dog components
+                dogController.GetComponent<Movement>().enabled = false;
+                dogCamera.SetActive(false);
+                break;
+        }
+    }
+}
