cc6b0becefdd64ddc385801e7148e4357991f996
Dana Woltz <dw952717@ohio.edu>
2020-10-03 23:12:55 -0400

    idfk

---
+++ Assets/DialogueTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    //public GameObject _Ghost;
+    public Canvas dialogue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            dialogue.enabled = true;
+            Debug.Log("TextEnabled");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            dialogue.enabled = false;
+        }
+    }
+}
