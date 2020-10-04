8b8a5e5a54fdc38c487e3ef4e91d57bda4f65a93
Kaeden <ks679318@ohio.edu>
2020-10-03 21:20:27 -0400

    DOG INTERACTION WORKS and is mutually exclusive to the player

--- Assets/Scripts/Player/CameraRay.cs
+++ Assets/Scripts/Player/CameraRay.cs
@@ -6,12 +6,12 @@ public class CameraRay : MonoBehaviour
 {
     public Camera PlayerCamera;
     RaycastHit hit;
-    LayerMask ThisLayer;
+    public LayerMask ThisLayer;
     
     void Update()
     {
         Ray ray = PlayerCamera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit) && Input.GetButtonDown("Interact") && hit.transform.gameObject.tag == "Interactable")
+        if (Physics.Raycast(ray, out hit, ThisLayer) && Input.GetButtonDown("Interact") && hit.transform.gameObject.tag == "Interactable")
         {
             GameObject objectHit = hit.transform.gameObject;
             switch (hit.transform.gameObject.name)
